c68147e9e00adf9dc14d17583bc61263e43d9dc2
suhongkim98 <kshz9815@naver.com>
2021-05-08 02:26:31 +0900

    라우팅 경로 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { Route, Switch } from 'react-router';
 import Login from 'routes/Login';
+import ManageCategory from 'routes/ManageCategory';
+import ManageNotice from 'routes/ManageNotice';
+import ManageItem from 'routes/ManageItem';
 import NotFound from 'routes/NotFound';
 import Register from 'routes/Register';
 import Home from './routes/Home';
@@ -10,6 +13,9 @@ const App = () => {
       <Route exact path="/" component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
+      <Route path="/category" component={ManageCategory} />
+      <Route path="/notice" component={ManageNotice} />
+      <Route path="/item" component={ManageItem} />
       <Route path="*" component={NotFound} />
     </Switch>
   );
