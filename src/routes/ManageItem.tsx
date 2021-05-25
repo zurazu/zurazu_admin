13717b249869f603ff1a8102fc29d4b8c0bbe5ed
suhongkim98 <kshz9815@naver.com>
2021-05-26 01:42:12 +0900

    판매 신청 페이지 이전 다음 버튼 구현

--- src/routes/ManageItem.tsx
+++ src/routes/ManageItem.tsx
@@ -3,11 +3,42 @@ import React, { useEffect, useState } from 'react';
 import MainTemplate from 'components/MainTemplate';
 import { axiosApiInstance } from 'modules/axiosApiInstance';
 import ProductItem from 'components/ProductItem';
+import styled from 'styled-components';
 
+
+const ButtonWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+`;
+const PrevNextButton = styled.button`
+    width: 200px;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid black;
+    border-radius: 5px;
+`;
 const ManageItem = (props: any) => {
     const [productList, setProductList] = useState([]);
+    const [offset,setOffset] = useState(0);
+    const [limit, setLimit] = useState(10);
+
+    const onClickPrevButton = () => {
+        if(offset - 10 < 0){
+            setOffset(0);
+        } else {
+            setOffset(offset - 10);
+        }
+    }
+    const onClickNextButton = () => {
+        setOffset(offset + 10);
+    }
+
     useEffect(() => {
-        axiosApiInstance.get("/admin/applySellProduct?offset=0&limit=10").then((response) => {
+        axiosApiInstance.get("/admin/applySellProduct?offset=" + offset + "&limit=" + limit).then((response) => {
             console.log(response.data.list);
             const list = response.data.list.products.map((row: any) => {
                 return <ProductItem key={row.idx} productIdx={row.idx} brandName={row.brandName} date={row.date} saleStatus={row.saleStatus} orderNumber={row.orderNumber} />;
@@ -16,10 +47,14 @@ const ManageItem = (props: any) => {
         }).catch((error) => {
             alert("오류");
         });
-    }, []);
+    }, [offset, limit]);
     
     return <MainTemplate history={props.history}>
         {productList}
+        <ButtonWrapper>
+            <PrevNextButton onClick={onClickPrevButton}>이전</PrevNextButton>
+            <PrevNextButton onClick={onClickNextButton}>다음</PrevNextButton>
+        </ButtonWrapper>
     </MainTemplate>;
 }
 
